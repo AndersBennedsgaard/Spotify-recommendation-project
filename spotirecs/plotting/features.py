7dae332fc312537d1fa6873dcb2053b1d9bec2d2
AndersBennedsgaard <abbennedsgaard@gmail.com>
2023-04-13 21:33:19 +0200

    update plotting method

--- spotirecs/plotting/features.py
+++ spotirecs/plotting/features.py
@@ -1,26 +1,32 @@
+from typing import Optional
+
 import matplotlib.pyplot as plt
-import pandas as pd
+import numpy as np
 
 
-def plot_features(column_names: list[str], data: pd.DataFrame):
+def plot_features(
+    column_names: list[str],
+    data: np.ndarray,
+    secondary_data: Optional[np.ndarray] = None,
+):
     """Make a plot of features extracted from a list of tracks in Spotify"""
 
+    if secondary_data is not None and data.shape[1] != secondary_data.shape[1]:
+        raise IndexError("Data of different dimensions")
+
     fig, axes = plt.subplots(
-        len(column_names), len(column_names), figsize=(len(column_names) * 2, len(column_names) * 2)
+        len(column_names),
+        len(column_names),
+        figsize=(len(column_names) * 2, len(column_names) * 2),
     )
     for i, col_i in enumerate(column_names):
         for j, col_j in enumerate(column_names):
             axis: plt.Axes = axes[j, i]  # type: ignore
-            axis.scatter(
-                data.loc[data['rating'] == 1][col_j],
-                data.loc[data['rating'] == 1][col_i],
-                s=1, c='tab:blue'
-            )
-            axis.scatter(
-                data.loc[data['rating'] == 0][col_j],
-                data.loc[data['rating'] == 0][col_i],
-                s=1, c='tab:orange'
-            )
+            axis.scatter(data[:, i], data[:, j], s=1, c="tab:blue")
+            if secondary_data is not None:
+                axis.scatter(
+                    secondary_data[:, i], secondary_data[:, j], s=1, c="tab:orange"
+                )
             axis.set_xticks([])
             axis.set_xticklabels([])
             axis.set_yticks([])
@@ -29,7 +35,7 @@ def plot_features(column_names: list[str], data: pd.DataFrame):
                 axis.set_ylabel(col_j)
             if j == 0:
                 axis.set_xlabel(col_i)
-                axis.xaxis.set_label_position('top')
+                axis.xaxis.set_label_position("top")
 
     # Remove white-space between subplots
     plt.subplots_adjust(wspace=0, hspace=0)
